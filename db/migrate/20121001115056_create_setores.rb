5d244358da189c3fb63cc25119e95cb0aee6c577
Heber Nunomura <heber28@gmail.com>
2021-03-17 19:35:09 -0300

    setores e usuarios da diretoria de transito

--- db/migrate/20121001115056_create_setores.rb
+++ db/migrate/20121001115056_create_setores.rb
@@ -6,25 +6,25 @@ class CreateSetores < ActiveRecord::Migration
 
       t.timestamps
     end
-    Setor.create :nome => 'Apoio Jari' # 1
-    Setor.create :nome => 'Arquivo Geral' # 2
-    Setor.create :nome => 'Assessoria Juridica' # 3
-    Setor.create :nome => 'Assessoria Juridica Dir de Trânsito' # 4
-    Setor.create :nome => 'Contabilidade' # 5
-    Setor.create :nome => 'Coordenadoria de Multas' # 6
-    Setor.create :nome => 'Coordenadoria de Sistema Viário' # 7
-    Setor.create :nome => 'Coordenadoria de Trânsito' # 8
-    Setor.create :nome => 'Coordenadoria Financeira' # 9
-    Setor.create :nome => 'Defesa da Autuação' # 10
-    Setor.create :nome => 'Diretoria Administrativa/Financeira' # 11
-    Setor.create :nome => 'Diretoria de Operações' # 12
-    Setor.create :nome => 'Diretoria de Trânsito' # 13
-    Setor.create :nome => 'Fiscalização Eletrônica' # 14
-    Setor.create :nome => 'Gerencia de Fiscalização de Postura' # 15
-    Setor.create :nome => 'Gerencia de Fiscalização de Trânsito' # 16
-    Setor.create :nome => 'Junda Admin de Recursos de Infrações' # 17
-    Setor.create :nome => 'Licitação' # 18
-    Setor.create :nome => 'Processamento de Multas' # 19
-    Setor.create :nome => 'Protocolo de Multas' # 20
+    Setor.create :nome => 'APOIO JARI' # 1
+    Setor.create :nome => 'ARQUIVO GERAL' # 2
+    Setor.create :nome => 'ASSESSORIA JURIDICA' # 3
+    Setor.create :nome => 'ASSESSORIA JURIDICA DIR DE TRANSITO' # 4
+    Setor.create :nome => 'CONTABILIDADE' # 5
+    Setor.create :nome => 'COORDENADORIA DE MULTAS' # 6
+    Setor.create :nome => 'COORDENADORIA DE SISTEMA VIARIO' # 7
+    Setor.create :nome => 'COORDENADORIA DE TRANSITO' # 8
+    Setor.create :nome => 'COORDENADORIA FINANCEIRA' # 9
+    Setor.create :nome => 'DEFESA DA AUTUACAO' # 10
+    Setor.create :nome => 'DIRETORIA ADMINISTRATIVA/FINANCEIRA' # 11
+    Setor.create :nome => 'DIRETORIA DE OPERACOES' # 12
+    Setor.create :nome => 'DIRETORIA DE TRANSITO' # 13
+    Setor.create :nome => 'FISCALIZACAO ELETRONICA' # 14
+    Setor.create :nome => 'GERENCIA DE FISCALIZACAO DE POSTURA' # 15
+    Setor.create :nome => 'GERENCIA DE FISCALIZACAO DE TRANSITO' # 16
+    Setor.create :nome => 'JUNTA ADMIN DE REUCRSOS DE INFRACOES' # 17
+    Setor.create :nome => 'LICITACAO' # 18
+    Setor.create :nome => 'PROCESSAMENTO DE MULTAS' # 19
+    Setor.create :nome => 'PROTOCOLO DE MULTAS' # 20
   end
 end
--- db/migrate/20121012185341_create_usuarios.rb
+++ db/migrate/20121012185341_create_usuarios.rb
@@ -17,7 +17,60 @@ class CreateUsuarios < ActiveRecord::Migration
                    :administrador => true,
                    :autor => true,
                    :desativado => false,
-                   :pode_alterar_setor => 1
+                   :pode_alterar_setor => false
 
+    Usuario.create :login => 'paulo.campaneli',
+                   :password => 'P@ssw0rd1',
+                   :password_confirmation => 'P@ssw0rd1',
+                   :nome => 'Paulo Campaneli',
+                   :administrador => false,
+                   :autor => true,
+                   :desativado => false,
+                   :pode_alterar_setor => false
+
+    Usuario.create :login => 'paulo.cesar',
+                   :password => 'P@ssw0rd1',
+                   :password_confirmation => 'P@ssw0rd1',
+                   :nome => 'Paulo Cesar',
+                   :administrador => false,
+                   :autor => true,
+                   :desativado => false,
+                   :pode_alterar_setor => false
+
+    Usuario.create :login => 'cristiano.bragatto',
+                   :password => 'P@ssw0rd1',
+                   :password_confirmation => 'P@ssw0rd1',
+                   :nome => 'Cristiano Bragatto',
+                   :administrador => false,
+                   :autor => true,
+                   :desativado => false,
+                   :pode_alterar_setor => false
+
+    Usuario.create :login => 'marcelo.pereira',
+                   :password => 'P@ssw0rd1',
+                   :password_confirmation => 'P@ssw0rd1',
+                   :nome => 'Marcelo Pereira',
+                   :administrador => false,
+                   :autor => true,
+                   :desativado => false,
+                   :pode_alterar_setor => false
+
+    Usuario.create :login => 'julio.costa',
+                   :password => 'P@ssw0rd1',
+                   :password_confirmation => 'P@ssw0rd1',
+                   :nome => 'Julio Cesar Costa',
+                   :administrador => false,
+                   :autor => true,
+                   :desativado => false,
+                   :pode_alterar_setor => false
+
+    Usuario.create :login => 'heber.nunomura',
+                   :password => 'P@ssw0rd1',
+                   :password_confirmation => 'P@ssw0rd1',
+                   :nome => 'Heber Nunomura',
+                   :administrador => false,
+                   :autor => true,
+                   :desativado => false,
+                   :pode_alterar_setor => false
   end
 end
