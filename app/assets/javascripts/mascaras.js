21af20c8a144a0affb75939c6dd2b7e386ae8e96
Heber Nunomura <heber28@gmail.com>
2021-03-24 08:40:08 -0300

    pequenas alterações

--- app/assets/javascripts/mascaras.js
+++ app/assets/javascripts/mascaras.js
@@ -4,5 +4,6 @@ jQuery(function ($) {
     $('#cpf').mask('999.999.999-99');
     $('#cnpj').mask('99.999.999/9999-99');
     $('#datetimepicker').mask('99/99/9999 99:99');
+    $('.data').mask('99/99/9999');
     $('.time').mask('99:99');
 });
