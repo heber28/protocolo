3c4ac85dc90761d3018ce17d3ae34c5c31429775
Heber Nunomura <heber28@gmail.com>
2021-03-30 16:10:45 -0300

    correção do teste

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,10 +5,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login com informacoes invalidas" do
     get login_path
     assert_template 'sessions/new'
-    post sessions_path, session: { login: "", password: "" }
+    post sessions_path, session: { usuario: "", password: "", setor: "" }
     assert_template 'sessions/new'
-    assert_false flash.empty?
-    get root_path
-    assert flash.empty?
+    assert_equal 'Usuário ou senha incorreta', flash[:alert]
   end
+
 end
