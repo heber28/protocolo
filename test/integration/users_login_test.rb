e4b45eabc80f16850c12df048270348862b0fabc
Heber Nunomura <heber28@gmail.com>
2021-04-04 15:03:48 -0300

    correção do teste

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,9 +5,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login com informacoes invalidas" do
     get login_path
     assert_template 'sessions/new'
-    post sessions_path, session: { usuario: "", password: "", setor: "" }
+    post sessions_path, usuario: "", password: "", setor: "" 
     assert_template 'sessions/new'
     assert_equal 'Usuário ou senha incorreta', flash[:alert]
   end
+
+  test "login com informacoes validas" do
+    get login_path
+    assert_template 'sessions/new'
+    #post sessions_path, usuario: "admin", password: "P@ssw0rd1", setor: "1" 
+    post_via_redirect '/sessions', :usuario => usuarios(:one).login, :password => 'P@ssw0rd1', :setor => '1'
+    assert_equal '/processos', path
+    assert_equal 'Logado!', flash[:notice]
+  end
+
 
 end
